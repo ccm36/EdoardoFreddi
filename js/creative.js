c72bf8fdb9920095b29ac2669397c36b23396cef
Cameron Malloy <cameron.c.malloy@gmail.com>
2020-02-11 10:47:47 +0900

    Update PA wines, minify CSS,JS

--- js/creative.js
+++ js/creative.js
@@ -75,4 +75,20 @@
     }
   });
 
+    // Magnific popup calls
+    $('.progettiAgricoli-portfolio').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
+    });
+
 })(jQuery); // End of use strict
